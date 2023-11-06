99b3b80f01a40a6a633dfab8df12e0163259df67
kome1 <101552871+kome1208@users.noreply.github.com>
2023-11-06 13:14:42 +0900

    ???

--- interactions/commands/news.js
+++ interactions/commands/news.js
@@ -147,7 +147,7 @@ module.exports = {
             repo: 'Misaka_Network',
             branch: 'main'
         });
-        const file = await git.getFile('/Server/News.json');
+        const file = await git.getFile('Server/News.json');
         const newsContent = JSON.parse((await git.getBlob(file.sha)));
         const tweaks = [
             ...interaction.options.data[0].options.filter((option) => option.name.startsWith('tweak')).map((tweak) => tweak.value),
