f904548b98a69ca1775304381c4a3eb6802b5236
kome1208 <kome1208@outlook.jp>
2024-01-05 05:28:45 +0900

    Added option to select the released version

--- interactions/commands/download.js
+++ interactions/commands/download.js
@@ -4,24 +4,35 @@ const axios = require('axios');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('download')
-		.setDescription('Get the latest release of misaka'),
+		.setDescription('Get the release of misaka')
+        .addStringOption(option =>
+            option.setName('version')
+            .setDescription('select a version')
+            .setAutocomplete(true)
+        ),
 	async execute(interaction) {
         await interaction.deferReply();
-        const { data } = await axios.get('https://api.github.com/repos/straight-tamago/misaka/releases/latest', {
-            headers: {
-                "Accept": "application/vnd.github+json",
-                "Authorization": "Bearer " + process.env['GITHUB_TOKEN'],
-                "X-GitHub-Api-Version": "2022-11-28"
-            }
-        });
-        const embed = new EmbedBuilder()
-        .setTitle(data.name)
-        .setURL(data.html_url)
-        .setDescription(data.body)
-        .addFields(
-            { name: "Files", value: data.assets.map((file) => `[${file.name}](${file.browser_download_url})`).join("\n")}
-        )
-        .setTimestamp(new Date(data.published_at));
-        await interaction.editReply({ embeds: [embed] });
+        const release_id = interaction.options.getString('version');
+        try {
+            const { data } = await axios.get(`https://api.github.com/repos/straight-tamago/misaka/releases/${release_id || 'latest'}`, {
+                headers: {
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": "Bearer " + process.env['GITHUB_TOKEN'],
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+            });
+            const embed = new EmbedBuilder()
+            .setTitle(data.name)
+            .setURL(data.html_url)
+            .setDescription(data.body)
+            .addFields(
+                { name: "Files", value: data.assets.map((file) => `[${file.name}](${file.browser_download_url})`).join("\n")}
+            )
+            .setTimestamp(new Date(data.published_at));
+            await interaction.editReply({ embeds: [embed] });
+        } catch (err) {
+            console.error(err);
+            await interaction.editReply({ content: 'An error occured.' });
+        }
     }
 }
