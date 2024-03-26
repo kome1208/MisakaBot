7321f020af287ef57b7101d4cffda6a86b8f67bf
kome1208 <kome1208@outlook.jp>
2024-03-26 23:53:16 +0900

    c

--- interactions/autocompletes/tweak.js
+++ interactions/autocompletes/tweak.js
@@ -6,7 +6,7 @@ module.exports = {
         const focused = interaction.options.getFocused(true);
         if (focused.name.startsWith('query')) {
             if (focused.value.length < 2) return;
-            const { data } = await axios.get(`https://misaka-search-ydkr.koyeb.app/api/v2/tweaks/search?q=${encodeURIComponent(focused.value)}`);
+            const { data } = await axios.get(`https://api.kome1.xyz/v2/tweaks/search?q=${encodeURIComponent(focused.value)}`);
             const list = data.tweaks.map((tweak) => ({ name: tweak.name, value: tweak.packageid }));
             await interaction.respond(list.slice(0, 25));
         }
